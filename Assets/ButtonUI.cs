3a89603ee2833544dcd5815db276f90da2e0d4bd
KarmaCc <113114243+KarmaCc@users.noreply.github.com>
2022-12-01 15:34:17 +0000

    hhghghgghg

---
+++ Assets/ButtonUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonUI : MonoBehaviour
+{
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
